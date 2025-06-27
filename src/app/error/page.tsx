92a1a444cb52d2febe6750469bf9be4624abcf2a
Steponass <steponas.dabuzinskas@gmail.com>
2025-06-27 16:38:03 +0200

    Set up Supabase Server-Side auth as per: https://supabase.com/docs/guides/auth/server-side/nextjs?queryGroups=router&router=app

---
+++ src/app/error/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function ErrorPage() {
+  return (
+    <main>
+      <h1>Someone did a oopsie 💩</h1>
+      <p>
+        Try again, maybe? You can also{" "}
+        <a href="mailto:steponas.dabuzinskas@gmail.com">email Step</a>
+      </p>
+    </main>
+  );
+}
---
+++ src/app/private/page.tsx
@@ -0,0 +1,19 @@
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+
+export default async function PrivatePage() {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/login')
+  }
+
+  return (
+    <main>
+      <h1>Hello, Clarise.</h1>
+      <p>Just kidding, hey {data.user.email}</p>
+    </main>
+  )
+}
