90a94bf4d39aa1d1dc867b05c849681b0343c776
Steponass <steponas.dabuzinskas@gmail.com>
2025-06-27 21:14:41 +0200

    Styles: 1) Added css reset 2) Made <main> permanent layout with flex grow to fit the screen between Header/Footer

---
+++ src/app/components/layout/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import classes from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={classes.footer}>
+        <div><p>Help is coming…</p></div>
+        <div><p>…Someday.</p></div>
+
+    </footer>
+  )
+}
--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -2,12 +2,12 @@
 
 export default function ErrorPage() {
   return (
-    <main>
+    <div>
       <h1>Someone did a oopsie 💩</h1>
       <p>
         Try again, maybe? You can also{" "}
         <a href="mailto:steponas.dabuzinskas@gmail.com">email Step</a>
       </p>
-    </main>
+    </div>
   );
 }
